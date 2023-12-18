4382e96f8dd58818d53c201cf76644a555e84a62
Sören Meier <soeren@s-me.ch>
2023-12-18 02:11:49 +0100

    Publish first version with some animations

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { resolve, relative } from 'path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { usedSsrComponents } from 'fire-svelte/ssr/components.js';
+import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ outDir, mode, ssrBuild }) => {
 	return {
 		plugins: [
 			svelte({
+				preprocess: sveltePreprocess(),
 				compilerOptions: {
 					hydratable: true
 				}
